84501102c33c0fae496734cd51a04c3d658f62f8
raquelgarza <raquelgarza95@gmail.com>
2026-02-02 13:51:45 +0100

    fix: paths in methylartist scripts

--- ONT_invitro/processing/methylartist_linc00662_locus_hg38_fetal.sh
+++ ONT_invitro/processing/methylartist_linc00662_locus_hg38_fetal.sh
@@ -14,11 +14,11 @@ module load OpenMPI/4.1.1
 module load methylartist/1.2.6
 
 # Define variables
-BAM_1="/scale/gr01/shared/jjakobsson/backup/op_065/1_mapping/hg38/MN10_Control_w11_Rep1_ontDNA_fetal_human_basecalls_5mC_5hmC_hg38.sorted.bam"
-BAM_2="/scale/gr01/shared/jjakobsson/backup/op_065/1_mapping/hg38/MN10_Control_w7_Rep1_ontDNA_fetal_human_basecalls_5mC_5hmC_hg38.sorted.bam"
-#ANNOTATION_FILE="/scale/gr01/home/patger/backup/genomes/hg38/hg38.ncbiRefSeq.sorted.gtf.gz"
-ANNOTATION_FILE="/scale/gr01/shared/jjakobsson/backup/rawdatasets_2024/MN01_XDPtissue/gencode.v46.annotation.sorted.gtf.gz"
-REFERENCE_GENOME="/scale/gr01/shared/common/genome/genomes/human/hg38/hg38.fa"
+BAM_1="/op_065/1_mapping/hg38/MN10_Control_w11_Rep1_ontDNA_fetal_human_basecalls_5mC_5hmC_hg38.sorted.bam"
+BAM_2="/op_065/1_mapping/hg38/MN10_Control_w7_Rep1_ontDNA_fetal_human_basecalls_5mC_5hmC_hg38.sorted.bam"
+#ANNOTATION_FILE="hg38.ncbiRefSeq.sorted.gtf.gz"
+ANNOTATION_FILE="gencode.v46.annotation.sorted.gtf.gz"
+REFERENCE_GENOME="hg38.fa"
 
 # +/- 10kb adjusted to chimp locus size (30364 bp)
 methylartist locus -b ${BAM_2}:#94b3d1,${BAM_1}:#c1c8e8 -i chr19:27777035-27807399 -g ${ANNOTATION_FILE} -p 1,8,1,1,2 --ref ${REFERENCE_GENOME} --motif CG -m m --labelgenes --svg
--- ONT_invitro/processing/methylartist_linc00662_locus_pantro6_org_ips.sh
+++ ONT_invitro/processing/methylartist_linc00662_locus_pantro6_org_ips.sh
@@ -14,12 +14,12 @@ module load OpenMPI/4.1.1
 module load methylartist/1.2.6
 
 # Define variables
-BAM_1="/scale/gr01/shared/jjakobsson/backup/op_065/1_mapping/pantro6/MN10_WT_Rep1_ontDNA_iPSC_Sandra_chimp_basecalls_5mC_5hmC_panTro6.sorted.bam"
-BAM_2="/scale/gr01/shared/jjakobsson/backup/op_065/1_mapping/pantro6/MN10_WT_Rep1_ontDNA_iPSC_JoC_chimp_basecalls_5mC_5hmC_panTro6.sorted.bam"
-BAM_3="/scale/gr01/shared/jjakobsson/backup/MN10_ontDNA/1_mapping/panTro6/MN10_WT_Rep1_ontDNA_org_Sandra_chimp_d15_basecalls_5mC_5hmC_panTro6.sorted.bam"
-BAM_4="/scale/gr01/shared/jjakobsson/backup/MN10_ontDNA/1_mapping/panTro6/MN10_WT_Rep1_ontDNA_org_JoC_chimp_d15_basecalls_5mC_5hmC_panTro6.sorted.bam"
-ANNOTATION_FILE="/scale/gr01/shared/jjakobsson/processed/panTro6/panTro6.ncbiRefSeq.sorted.gtf.gz"
-REFERENCE_GENOME="/scale/gr01/shared/jjakobsson/backup/nino_fs1_jakobssonlab/panTro6_genome/panTro6.fa"
+BAM_1="/op_065/1_mapping/pantro6/MN10_WT_Rep1_ontDNA_iPSC_Sandra_chimp_basecalls_5mC_5hmC_panTro6.sorted.bam"
+BAM_2="/op_065/1_mapping/pantro6/MN10_WT_Rep1_ontDNA_iPSC_JoC_chimp_basecalls_5mC_5hmC_panTro6.sorted.bam"
+BAM_3="/MN10_ontDNA/1_mapping/panTro6/MN10_WT_Rep1_ontDNA_org_Sandra_chimp_d15_basecalls_5mC_5hmC_panTro6.sorted.bam"
+BAM_4="/MN10_ontDNA/1_mapping/panTro6/MN10_WT_Rep1_ontDNA_org_JoC_chimp_d15_basecalls_5mC_5hmC_panTro6.sorted.bam"
+ANNOTATION_FILE="panTro6.ncbiRefSeq.sorted.gtf.gz"
+REFERENCE_GENOME="panTro6.fa"
 
 # +/- 10kb
 methylartist locus -b ${BAM_2}:#C1E5DB,${BAM_1}:#CEEADB,${BAM_4}:#AAE5D4,${BAM_3}:#B7EACE -i chr19:25693341-25723705 -g ${ANNOTATION_FILE} -l 25706377-25713292 -p 1,8,1,1,4 --ref ${REFERENCE_GENOME} --motif CG -m m --labelgenes --svg
